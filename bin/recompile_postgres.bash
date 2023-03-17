a1e11c9ceae0882ab961e50de2cfbcc0b5049937
Josh Curtis <jcurtis825@gmail.com>
2023-03-17 10:57:09 -0400

    Add script to quickly restart postgres

---
+++ bin/recompile_postgres.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd ~/code/postgres/ || exit 1
+make -j 8;
+make install;
+/opt/jc/postgresql/bin/pg_ctl restart -D /opt/jc/pgdata/ -l/tmp/jc_pg.log -o '-p 21800 -E -d 1 -c fsync=false -c full_page_writes=false -c synchronous_commit=off -c log_statement=none -c wal_level=logical'
+echo "done"
+exit 0
