888ff35ecc058f149ad5545e7e3a1408b411e6c9
Josh Curtis <jcurtis825@gmail.com>
2023-03-19 16:01:28 -0700

    Move code to experiment with better comments

--- src/main.rs
+++ src/main.rs
@@ -86,42 +86,47 @@ fn update_account_balance(client: &mut Client, a_id: i64, balance: i64) {
     ).unwrap();
 }
 
+fn experiment_insert_into_accounts_until_heapprune(client: &mut Client) {
+    reset_accounts_table(client);
+    let a_id = insert_into_accounts(client, "2y");
+    // count from 1 since we inserted a tuple above
+    // each update "inserts" 1 new tuple into the page, insert might be the wrong word
+    for num_tuples_inserted in 1..16 {
+        let balance = num_tuples_inserted; // use num tuples insert as balance to make inspection a bit easier
+        update_account_balance(client, a_id, balance)
+    }
+    // TOOD(josh): figure out the particular meaning, but this basically means that there's a HOT chain
+    // of 16 tuples in the page. The first 15 point to the next tuple and the last tuple is the "real" one
+    let acct_page_items = heap_page_items(client, "accounts", 0);
+    let num_tuples_in_page = acct_page_items.len();
+    assert_eq!(num_tuples_in_page, 16);
+    let lpflag_to_count = count_tuples_by_lpflag(acct_page_items);
+    assert_eq!(16, *lpflag_to_count.get(&1).unwrap());
+
+    update_account_balance(client, a_id, 17);
+    let acct_page_items = heap_page_items(client, "accounts", 0);
+    assert_eq!(acct_page_items.len(), 16);
+    let lpflag_to_count = count_tuples_by_lpflag(acct_page_items);
+    assert_eq!(13, *lpflag_to_count.get(&0).unwrap());
+    assert_eq!(2, *lpflag_to_count.get(&1).unwrap());
+    assert_eq!(1, *lpflag_to_count.get(&2).unwrap());
+    wait_for_enter()
+}
+
+fn count_tuples_by_lpflag(heap_page_items: Vec<HeapPageItem>) -> BTreeMap<i32, i32> {
+    heap_page_items.iter()
+        .fold(BTreeMap::new(), |mut map, item| {
+            let count = map.entry(item.lp_flags).or_insert(0);
+            *count += 1;
+            map
+    })
+}
+
 fn main() {
     println!("Hello, world!");
 
     let mut client = connect_to_local();
-    println!("Creating accounts");
-    reset_accounts_table(&mut client);
-    let a_id = insert_into_accounts(&mut client, "2y");
-    //println!("Got {}", a_id);
-
-    let tuples_to_fill_page = 160;
-
-    println!("updating balance");
-    // start from 1 b/c we already did an insert
-    for i in 1..tuples_to_fill_page {
-        update_account_balance(&mut client, a_id, i);
-        let heap_page_items = heap_page_items(&mut client, "accounts", 0);
-        // group heap_page_items by item.lp_flags
-        let lp_flag_to_count: BTreeMap<i32, i32> = heap_page_items.iter()
-            .fold(BTreeMap::new(),|mut lp_flag_to_count, item| {
-                let count = lp_flag_to_count.entry(item.lp_flags).or_insert(0);
-                *count += 1;
-                lp_flag_to_count
-            });
-        println!("--------");
-        lp_flag_to_count.iter().for_each(|(lp_flag, count)| {
-            println!("lp_flag: {} count: {}", lp_flag, count);
-        });
-
-
-        let lp_flags2_count = heap_page_items.iter().filter(|item| item.lp_flags == 2).count();
-        if lp_flags2_count > 0 {
-            println!("lp_flags2_count: {} at {} tuples", lp_flags2_count, i);
-            assert_eq!(i, 16);
-            break
-        }
-    }
+    experiment_insert_into_accounts_until_heapprune(&mut client);
 
     println!("----------------------------------------------------------------");
     println!("new experiment");
